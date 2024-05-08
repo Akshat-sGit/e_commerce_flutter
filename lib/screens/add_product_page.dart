f61ffdbc5eab071790970dd806cf1614016e35cc
Akshat-sGit <e21cseu0806@bennett.edu.in>
2024-05-08 20:09:03 +0530

    Created dropdown buttons

--- lib/screens/add_product_page.dart
+++ lib/screens/add_product_page.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
+import '../widgets/dropdown_btn.dart';
 
 class AddProductPage extends StatelessWidget {
   const AddProductPage({super.key});
@@ -78,6 +81,64 @@ class AddProductPage extends StatelessWidget {
                     fontWeight: FontWeight.w400,
                   ),
                 ),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    label: const Text("Image URL"),
+                    hintText: "Enter Image URL",
+                    hintStyle: GoogleFonts.poppins(
+                      color: const Color.fromARGB(142, 255, 255, 255),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    label: const Text("Price"),
+                    hintText: "Enter Product Price",
+                    hintStyle: GoogleFonts.poppins(
+                      color: const Color.fromARGB(142, 255, 255, 255),
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  style: GoogleFonts.poppins(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  children: [
+                    Flexible(child: DropDown(dropdownItem: "Category")),
+                    Flexible(child: DropDown(dropdownItem: "Size")),
+                  ],
+                ),
+                const SizedBox(height: 20),
+                Text("Order Product",
+                    style: GoogleFonts.poppins(
+                      color: Colors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    )),
+                const SizedBox(height: 20),
+                DropDown(dropdownItem: "Select Item"),
               ],
             ),
           ),
---
+++ lib/widgets/dropdown_btn.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class DropDown extends StatelessWidget {
+  final String dropdownItem;
+  DropDown({super.key, required this.dropdownItem});
+
+  final List<String> items = ['item1', 'item2', 'item3', 'item4']; // Data list
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: CupertinoButton(
+        color: Colors.green,
+        onPressed: () {
+          showModalBottomSheet<void>(
+            context: context,
+            builder: (BuildContext context) {
+              return CupertinoPicker(
+                scrollController: FixedExtentScrollController(
+                    initialItem: 0), // Set initial item
+                itemExtent: 40, // Height of each item
+                onSelectedItemChanged: (int index) {
+                  // Handle item selection (consider using a callback or state management)
+                  print(items[index]); // Example access to selected item
+                },
+                backgroundColor: Colors.transparent,
+                children: [
+                  for (String item in items)
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        item,
+                        style: GoogleFonts.poppins(
+                          fontSize: 14,
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                ],
+              );
+            },
+          );
+        },
+        child: Text(
+          dropdownItem,
+          style: GoogleFonts.poppins(
+            color: Colors.white,
+            fontSize: 10,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
