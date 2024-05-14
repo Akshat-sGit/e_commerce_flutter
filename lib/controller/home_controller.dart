cb94c56dfd2986fe0c3416c9cdf3f40c69a5e9dc
Akshat-sGit <e21cseu0806@bennett.edu.in>
2024-05-14 19:43:21 +0530

    chore: Add Firestore collection reference for products and implement addProduct method

--- lib/controller/home_controller.dart
+++ lib/controller/home_controller.dart
@@ -4,12 +4,18 @@ import 'package:get/get.dart';
 class HomeController extends GetxController {
   String test = 'Test String';
 
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  late CollectionReference productCollection;
+
   @override
   // ignore: unnecessary_overrides
   void onInit() {
+    productCollection = firestore.collection('products');
     super.onInit();
   }
 
-
-
+  addProduct() {
+    DocumentReference doc = productCollection.doc();
+    
+  }
 }
